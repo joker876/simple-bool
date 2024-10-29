b2e674ca9693dcd659546327f694f2a809e51c8f
joker876 <joker876x@gmail.com>
2024-10-29 15:20:20 +0100

    dual package experiments

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './dist/esm/index.js',
-    mode: 'production',
-    output: {
-        filename: 'simple-bool.min.js',
-        path: path.resolve(__dirname, 'dist/browser'),
-        library: 'SimpleBool',
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-        auxiliaryComment: 'A simple set of functions that return a boolean.',
-    },
+  entry: './dist/esm/index.mjs',
+  mode: 'production',
+  output: {
+    filename: 'simple-bool.min.js',
+    path: path.resolve(__dirname, 'dist/browser'),
+    library: 'SimpleBool',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    auxiliaryComment: 'A simple set of functions that return a boolean.',
+  },
 };
