29efb584d4dcadbbd14776bd85b5af5fcf86a3b4
joker876 <joker876x@gmail.com>
2024-10-29 15:32:53 +0100

    experiments

---
+++ scripts/prepare-package-json.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const distDir = './dist';
+(() => {
+  fs.readdir(distDir, function (err, dirs) {
+    if (err) {
+      throw err;
+    }
+    dirs.forEach(function (dir) {
+      if (dir === 'esm') {
+        var packageJsonFile = path.join(distDir, dir, '/package.json');
+        if (!fs.existsSync(packageJsonFile)) {
+          fs.writeFile(packageJsonFile, new Uint8Array(Buffer.from('{"type": "module"}')), function (err) {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+      }
+    });
+  });
+})();
